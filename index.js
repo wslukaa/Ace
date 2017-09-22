78aa961d23b6855a61a4418bfde8b447d4237a2a
WUFAN\dell <fwuae@connect.ust.hk>
2017-09-22 22:54:46 +0800

    Use heroku example to test first ;(

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-var domain = require('domain');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.get('/', function(req, res) {
-  console.log(2333);
-  res.render('index', { currentTime: new Date() });
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-app.post('/test', function(req, res) {
-  res.json({
-    aa: '2333',
-  });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
-
-module.exports = app;
