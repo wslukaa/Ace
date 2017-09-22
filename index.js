576cd3fb24f3e72463caee643285e41634d98e4f
WUFAN\dell <fwuae@connect.ust.hk>
2017-09-22 22:50:10 +0800

    Add views

--- index.js
+++ index.js
@@ -14,40 +14,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  var d = null;
-  if (process.domain) {
-    d = process.domain;
-  } else {
-    d = domain.create();
-  }
-  d.add(req);
-  d.add(res);
-  d.on('error', function(err) {
-    console.error('uncaughtException url=%s, msg=%s', req.url, err.stack || err.message || err);
-    if(!res.finished) {
-      res.statusCode = 500;
-      res.setHeader('content-type', 'application/json; charset=UTF-8');
-      res.end('uncaughtException');
-    }
-  });
-  d.run(next);
-});
-
 app.get('/', function(req, res) {
   console.log(2333);
   res.render('index', { currentTime: new Date() });
 });
 
-app.post('/api/teams', function(req, res) {
+app.post('/test', function(req, res) {
   res.json({
-    name: '233',
-    url: 'https://codeitsuisse.herokuapp.com/',
-    members: [
-      'Ng Argens',
-      'Winson Lok',
-      'WU Fan',
-    ],
+    aa: '2333',
   });
 });
 
