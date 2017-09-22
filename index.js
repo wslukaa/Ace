475bd2d6383d8cc160a62a91d1ec3d1963334675
WUFAN\dell <fwuae@connect.ust.hk>
2017-09-22 22:29:27 +0800

    The first test

---
+++ index.js
@@ -0,0 +1,53 @@
+var domain = require('domain');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  var d = null;
+  if (process.domain) {
+    d = process.domain;
+  } else {
+    d = domain.create();
+  }
+  d.add(req);
+  d.add(res);
+  d.on('error', function(err) {
+    console.error('uncaughtException url=%s, msg=%s', req.url, err.stack || err.message || err);
+    if(!res.finished) {
+      res.statusCode = 500;
+      res.setHeader('content-type', 'application/json; charset=UTF-8');
+      res.end('uncaughtException');
+    }
+  });
+  d.run(next);
+});
+
+app.get('/', function(req, res) {
+  res.render('index', { currentTime: new Date() });
+});
+
+app.post('/api/teams', function(req, res) {
+  res.json({
+    name: '233',
+    url: 'https://codeitsuisse.herokuapp.com/',
+    members: [
+      'Ng Argens',
+      'Winson Lok',
+      'WU Fan',
+    ],
+  });
+});
+
+module.exports = app;
