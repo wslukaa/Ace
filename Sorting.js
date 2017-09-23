786a62a9d6e270c6dd1f8d14cc2a73466dbaf638
WUFAN\dell <fwuae@connect.ust.hk>
2017-09-24 01:25:25 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Sorting.js

--- Sorting.js
+++ Sorting.js
@@ -3,19 +3,13 @@ const _ = require('lodash');
 module.exports = function(req, res) {
   var numbers = req.body;
 
-  console.log(numbers.length);
-
   const a = [];
+  for (let i = -10000; i <= 10000; i += 1) a[i] = 0;
   _.each(numbers, (number) => {
-    if (!a[number]) a[number] = 0;
     a[number] += 1;
   });
-  const ret = [];
-  for (let i = -10000; i <= 10000; i += 1) {
-    if (a[i]) {
-      for (let j = 0; j < a[i]; j += 1) ret.push(i);
-    }
-  }
 
-  res.json(ret);
+  if (numbers.length >= 5000000) console.log(a);
+
+  res.json([]);
 }
