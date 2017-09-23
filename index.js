6fc21c3dfc059ede2c239830ae27d7f7615cd3ed
Ng Argens <argens1203@gmail.com>
2017-09-24 01:07:54 +0800

    hr

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sort = require('./Sorting');
 const trainPlanner =require('./trainPlanner');
 const releaseSchedule = require('./Release Schedule');
 const hm = require ("./heistmodule")
-const hr = require ('./hrModule')
+const hr = require ('./hrModule2')
 
 const stringCompression = require('./String Compression');
 
