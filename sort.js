b621e68ca32ae829e0665c98c66dfb223c82612b
Winson Luk <wslukaa@ust.hk>
2017-09-23 12:46:49 +0800

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,13 +1,6 @@
 module.exports = function(req, res) {
-  var input = req.body;
-  var people = input;
-  people = people.sort(function(a, b) {
-      if (asc) {
-          return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
-      } else {
-          return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
-      }
-  });
-  showResults();
+  var points = req.body;
+  
+  points.sort(function(a, b){return a-b});
   res.json();
 }
