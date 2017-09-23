83867d0f63582e2c27e5863973b285b43de1ba53
WUFAN\dell <fwuae@connect.ust.hk>
2017-09-23 13:04:59 +0800

    no message

--- sort.js
+++ sort.js
@@ -1,9 +1,9 @@
 module.exports = function(req, res) {
   var numbers = req.body;
-  
+
   numbers.sort(function(a, b) {
-  return a - b;
-});
-console.log(numbers);
-  res.json();
+    return a - b;
+  });
+  console.log(numbers);
+  res.json(numbers);
 }
