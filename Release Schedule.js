0c3d5bcbdd922d5eb5779edc3a46d1603c706000
WUFAN\dell <fwuae@connect.ust.hk>
2017-09-23 20:55:47 +0800

    no message

--- Release Schedule.js
+++ Release Schedule.js
@@ -39,7 +39,7 @@ module.exports = (req, res) => {
       start: parseTime(b[1]).getTime(),
       end: parseTime(b[2]).getTime(),
     };
-    console.log(aa.start, aa.end);
+    console.log(b[1], new Date(aa.start), b[2], new Date(aa.end));
     tasks.push(aa);
     if (aa.start < end && aa.end > start && aa.start < aa.end) {
       times.push(aa.start);
