6e33e1ac71b55ad5ddf70c91f06d801fd60a9aca
Ng Argens <argens1203@gmail.com>
2017-09-24 08:33:26 +0800

    sort

--- Sorting.js
+++ Sorting.js
@@ -2,8 +2,8 @@ const _ = require('lodash');
 
 module.exports = function(req, res) {
   var numbers = req.body;
-
+  console.log (numbers);
   var result = numbers.sort ()
-
+  console.log (result);
   res.json(result);
 }
