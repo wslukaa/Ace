798df99f66de427cf3d900785e9c23fc22b6cbe8
Ng Argens <argens1203@gmail.com>
2017-09-24 12:14:37 +0800

    sort

--- Sorting.js
+++ Sorting.js
@@ -1,48 +1,45 @@
 const _ = require('lodash');
 var ts = require ('timsort')
 
-module.exports = function (input) {
-  var heapSort = (function () {
-    function heapify(array, index, heapSize) {
-        var left = 2 * index + 1,
-            right = 2 * index + 2,
-            largest = index;
-     
-        if (left < heapSize && array[left] > array[index])
-            largest = left;
+exports.result = function (input) {
+	var quickSort = (function () {
 
-        if (right < heapSize && array[right] > array[largest])
-            largest = right;
-     
-        if (largest !== index) {
-            var temp = array[index];
-            array[index] = array[largest];
-            array[largest] = temp;
-            heapify(array, largest, heapSize);
-        }
-    }
+	    function partition(array, left, right) {
+	        var cmp = array[right - 1],
+	            minEnd = left,
+	            maxEnd;
+	        for (maxEnd = left; maxEnd < right - 1; maxEnd += 1) {
+	            if (array[maxEnd] <= cmp) {
+	                swap(array, maxEnd, minEnd);
+	                minEnd += 1;
+	            }
+	        }
+	        swap(array, minEnd, right - 1);
+	        return minEnd;
+	    }
 
-    function buildMaxHeap(array) {
-        for (var i = Math.floor(array.length / 2); i >= 0; i -= 1) {
-            heapify(array, i, array.length);
-        }
-        return array;
-    }
+	    function swap(array, i, j) {
+	        var temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	        return array;
+	    }
+	    
+	    function quickSort(array, left, right) {
+	        if (left < right) {
+	            var p = partition(array, left, right);
+	            quickSort(array, left, p);
+	            quickSort(array, p + 1, right);
+	        }
+	        return array;
+	    }
 
-    return function (array) {
-        var size = array.length,
-            temp;
-        buildMaxHeap(array);
-        for (var i = array.length - 1; i > 0; i -= 1) {
-            temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
-            size -= 1;
-            heapify(array, 0, size);
-        }
-        return array;
-    };
-}());
+	    return function (array) {
+	        return quickSort(array, 0, array.length);
+	    };
+	}());
 
-  return heapSort (input);
+
+	console.log ("43");
+	return quickSort (input);
 }
--- index.js
+++ index.js
@@ -24,12 +24,16 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.json({
+var jsonParser = bodyParser.json({
   limit: '50mb',
-}));
-app.use (bodyParser.urlencoded({
-  extended: true,
-}));
+});
+
+// app.use(bodyParser.json({
+//   limit: '50mb',
+// }));
+// app.use (bodyParser.urlencoded({
+//   extended: true,
+// }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -49,43 +53,164 @@ app.post('/test', function(req, res) {
   });
 });
 
-app.post('/determinant', function(req, res) {
+app.post('/determinant', jsonParser, function(req, res) {
   let matrix = math.matrix(req.body);
   res.json(math.det(matrix));
 });
 
-app.post('/heist', function (req, res){
+app.post('/heist', jsonParser,function (req, res){
 	res.json (hm.grab (req.body));
 });
-app.post('/releaseSchedule', releaseSchedule);
+app.post('/releaseSchedule', jsonParser, releaseSchedule);
 
 var storage = ""
-app.post ('/horse-racing', function (req, res, next){
+app.post ('/horse-racing', jsonParser, function (req, res, next){
   if (storage == ""){
     storage = req.body;
   }
 	res.json (hr.result (req.body));
 });
 
-app.post ('/getData', function (req, res, next){
+app.post ('/getData', jsonParser, function (req, res, next){
   res.json (storage);
 });
 
-app.post('/trainPlanner',trainPlanner);
+app.post('/trainPlanner',jsonParser, trainPlanner);
 
 app.post('/sort', function (req, res, next){
-  res.json (sort (req.body));
+  var data = '';
+  var last = 0;
+  var negative = false;
+  var arr = [];
+  var lastestArr = [];
+
+  req.on( 'data', function( chunk ) {
+    console.log (typeof chunk);
+    console.log (chunk.length);
+
+    var temp = [];
+    for (var i in chunk){
+      var num = chunk [i] - 48;
+      // console.log (num);
+      if (num == -3){
+        negative = true;
+        continue;
+      }
+      if (num >= 0 && num <= 9){
+        last = last * 10 + num;
+        continue;
+      }
+      if (num == -4 || num == 45){
+          if (negative) {temp.push (-1 * last);} else {temp.push (last);}
+          negative = false;
+          last = 0;
+        }
+        continue;
+      }
+
+      if (arr.length == 0){
+        // arr = temp;
+        arr = temp;
+      }
+      else
+      {
+        lastestArr = temp;
+      }
+
+      console.log ("117: " + arr.toString ());
+      console.log ("118: " + lastestArr.toString ());
+
+      if (lastestArr.length == 0)
+      {
+        arr = sort.result (arr);
+      }
+      else
+      {
+        var index = 0;
+        while (index < lastestArr.length){
+
+          var left = -1;
+          var right = arr.length;
+          var pivot = Math.floor (arr.length / 2);
+
+          // if (lastestArr [index] < arr [0]){
+          //   arr.shift (lastestArr [index]);
+          //   index ++;
+          //   continue;
+          // }
+
+          // if (lastestArr [index] > arr [arr.length - 1]){
+          //   arr.push (lastestArr [index]);
+          //   index ++;
+          //   continue;
+          // }
+
+          
+          while (true){
+
+            console.log ("" + index + " " + left + " " + right + " " + pivot);
+
+            if (lastestArr [index] < arr [pivot]){
+              right = pivot;
+              if (right == left + 1){
+                pivot = right;
+                break;
+              }
+              pivot = Math.floor ((left + pivot) / 2);
+              continue;
+            }
+            if (lastestArr [index] > arr [pivot]){
+              left = pivot;
+              if (right == left + 1){
+                pivot = left + 1;
+                break;
+              }
+              pivot = Math.floor ((right + pivot) / 2);
+              continue;
+            }
+            break;
+          }
+
+          arr.splice (pivot, 0, lastestArr [index])
+          console.log (arr.toString ());
+
+          index ++;
+          }
+        }
+
+    console.log ("Hello?");
+    console.log (arr.toString());
+
+    // if (chunk [0] == '['){
+    //   chunk.shift ();
+    // }
+    // if (chunk [chunk.length -1] == ']'){
+    //   chunk.pop ();
+    // }
+    // data += chunk;
+    // console.log (data);
+  });
+
+  req.on( 'end', function() {
+    // req.rawBody = data;
+    // console.log( 'on end: ', data )
+    // if ( data && data.indexOf( '{' ) > -1 ) {
+    //   req.body = JSON.parse( data );
+    // }
+    // next();
+    res.json (arr);
+  });
 });
 
-app.post('/releaseSchedule', releaseSchedule);
+app.post('/releaseSchedule', jsonParser, releaseSchedule);
 
 // app.post('/stringcompression/RLE', stringCompression.RLE);
-app.post('/stringcompression/RLE', stringComp.RLE);
-app.post('/stringcompression/LZW', stringComp.LZW);
-app.post('/stringcompression/WDE', stringComp.WDE);
+app.post('/stringcompression/RLE', jsonParser, stringComp.RLE);
+app.post('/stringcompression/LZW', jsonParser, stringComp.LZW);
+app.post('/stringcompression/WDE', jsonParser, stringComp.WDE);
 
-app.post('/calculateemptyarea', calculateEmptyArea);
+app.post('/calculateemptyarea', jsonParser, calculateEmptyArea);
 
-app.post('/mini-exchange', miniExchange);
+app.post('/mini-exchange', jsonParser, miniExchange);
 
-app.post('/warehouse-keeper/game-start', warehouseKeeper);
+app.post('/warehouse-keeper/game-start', jsonParser, warehouseKeeper);
