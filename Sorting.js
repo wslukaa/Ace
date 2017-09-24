1f57d91981819eeee6540699eb954811b4e1b60f
Ng Argens <argens1203@gmail.com>
2017-09-24 09:35:32 +0800

    sort

--- Sorting.js
+++ Sorting.js
@@ -2,8 +2,9 @@ const _ = require('lodash');
 
 module.exports = function(req, res) {
   var numbers = req.body;
+
   console.log (numbers);
-  var result = numbers.sort ()
+  var result = numbers.sort (function (a,b) {return a - b;});
   console.log (result);
   res.json(result);
 }
