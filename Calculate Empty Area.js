13d54e947a82474d930e53a6901a8247d884a236
Winson Luk <wslukaa@ust.hk>
2017-09-24 10:26:50 +0800

    Update Calculate Empty Area.js

--- Calculate Empty Area.js
+++ Calculate Empty Area.js
@@ -75,14 +75,14 @@ function find_angle(Ax,Ay,Bx,By,Cx,Cy) {
     var AC = Math.sqrt(Math.pow(Cx-Ax,2)+ Math.pow(Cy-Ay,2));
     return Math.acos((BC*BC+AB*AB-AC*AC)/(2*BC*AB));
 }
-   if (c.x > x0 && c.x<x1 && c.y < y0 && c.y1 > y0 && c.y1 < y1){
+   if (c.x0 > x0 && c.x1<x1 && c.y < y0 && c.y1 > y0 && c.y1 < y1){
      var AX = Math.sqrt(c.r*c.r-(y0-c.y)*(y0-c.y));
      var area_traingle = AX*(y0-c.y);
      var angle= find_angle(c.x-AX,y0,c.x,c.y,c.x+AX,y0);
      res.json(s-(Math.round((Math.PI * c.r * c.r*angle/(2*Math.PI)) * 100) / 100-area_traingle));
     return;
   }
-    if (c.x > x0 && c.x<x1 && c.y > y1 && c.y0 < y1 && c.y0>y0){
+    if (c.x0 > x0 && c.x1<x1 && c.y > y1 && c.y0 < y1 && c.y0>y0){
      var AX = Math.sqrt(c.r*c.r-(c.y-y1)*(c.y-y1));
      var area_traingle = AX*(c.y-y1);
      var angle= find_angle(c.x-AX,y1,c.x,c.y,c.x+AX,y1);
